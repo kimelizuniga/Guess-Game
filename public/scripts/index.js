8d7d82d07142810c0b27e0678003eab25d6c7dbb
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-06-18 21:21:32 -0400

    fix responsiveness

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -220,16 +220,6 @@ window.onclick = function(event) {
 // submitName.addEventListener('click', addScore)
 
 
-
-switch (document.readyState){
-    case 'complete':
-        setTimeout(function () {
-            let viewheight = $(window).height();
-            let viewwidth = $(window).width();
-            let viewport = document.querySelector("meta[name=viewport]");
-            viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
-        }, 300);
-}
 // Event listeners //
 
 startButton.addEventListener('click', gameStart)
